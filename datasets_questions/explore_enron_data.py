2b2dad9b97fbff27b16fa4c04a07302028b0313b
talebisia <siavash.talebipour@gmail.com>
2019-07-17 15:29:10 +0100

    playing with the enron dataset

--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,41 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "rb"))
 
+# for key in enron_data:
+#     print(key)
+#
+# var = len({k: v for k, v in enron_data.items() if v["poi"] == 1})
+# print (var)
+
+# print (enron_data["PRENTICE JAMES"]["total_stock_value"])
+
+# print (enron_data["COLWELL WESLEY"]["from_this_person_to_poi"])
+
+# print (enron_data["SKILLING JEFFREY K"]["exercised_stock_options"])
+# print (enron_data["LAY KENNETH L"]["total_payments"])
+# print (enron_data["FASTOW ANDREW S"]["exercised_stock_options"])
+
+# var = len({k: v for k, v in enron_data.items() if v["salary"] != "NaN"})
+# var = len({k: v for k, v in enron_data.items() if v["salary"] != "NaN"})
+# print (var)
+
+# var = len({k: v for k, v in enron_data.items() if v["total_payments"] == "NaN"})
+# var1 = {k: v for k, v in enron_data.items() if v["total_payments"] == "NaN"}
+# print (var)
+# print (var1)
+# print ("people in the entire datset = " + str(len(enron_data.items())))
+
+# var = {k: v for k, v in enron_data.items() if v["total_payments"] == "NaN" and v["poi"] == True}
+# var1 = len({k: v for k, v in enron_data.items() if v["total_payments"] == "NaN" and v["poi"] == True})
+# print (var)
+# print (var1)
+
+
+var = len({k: v for k, v in enron_data.items() if v["poi"] == True})
+var1 = {k: v for k, v in enron_data.items() if v["poi"] == True}
+print (var)
+print (var1)
+print ("people in the entire datset = " + str(len(enron_data.items())))
+
+
 
